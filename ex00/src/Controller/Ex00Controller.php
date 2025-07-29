02be2654a97d1088d6fda01ba32c118e56e6607b
Winspear22 <adnen.aloui2@hotmail.com>
2025-07-29 20:22:35 +0200

    PHP05 -- ex00 : presque terminé, il reste à séparer la logique métier du controller.

--- ex00/src/Controller/Ex00Controller.php
+++ ex00/src/Controller/Ex00Controller.php
@@ -2,76 +2,68 @@
 
 namespace App\Controller;
 
+use Exception;
 use Doctrine\DBAL\Connection;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class Ex00Controller extends AbstractController
+final class Ex00Controller extends AbstractController
 {
-	const SUCCESS = 0;
-	const FAILURE = 1;
-	const DOES_NOT_EXIST = 2;
-
-	/**
-	 * @Route("/ex00", name="ex00_index")
-	 */
-	public function index(Connection $connection, Request $request): Response
-	{
-		$tableName = "user";
-
-		if ($request->isMethod('POST')) 
-		{
-			$doesTableExists = $this->tableExistenceCheck($tableName, $connection);
-			if ($doesTableExists['status'] === self::DOES_NOT_EXIST) 
-			{
-				$createTable = $this->createTable($tableName, $connection);
-				$this->addFlash('notice', $createTable['message']);
-			}
-			else
-				$this->addFlash('notice', $doesTableExists['message']);
-			return $this->redirectToRoute('ex00_index');
-		}
-		return $this->render('index.html.twig', [
-			'tableName' => $tableName
-		]);
-	}
-
-    private function tableExistenceCheck(string $tableName, Connection $connection): array
+    
+    /**
+     * @Route("/ex00", name="ex00_index")
+     */
+    public function index(): Response
     {
-		try
-		{
-			$doesTableExists = $connection->executeQuery("SHOW TABLES LIKE '$tableName'")->rowCount();
-			if ($doesTableExists > 0)
-				return (['status' => self::SUCCESS, 'message' => "La table $tableName existe déjà."]);
-			return (['status' => self::DOES_NOT_EXIST]);
-		}
-		catch (\Exception $e)
-		{
-			return ['status' => self::FAILURE, 'message' => "Erreur lors de la RECHERCHE de la table '$tableName', code erreur : " . $e->getMessage()];
-		}
-	}
+        return $this->render('ex00/index.html.twig', [
+            'controller_name' => 'Ex00Controller',
+        ]);
+    }
 
-	private function createTable(string $tableName, Connection $connection): array
-	{
-		try 
-		{
-			$sql = "CREATE TABLE IF NOT EXISTS `$tableName` (
-				id INT AUTO_INCREMENT PRIMARY KEY,
-				username VARCHAR(255) UNIQUE,
-				name VARCHAR(255),
-				email VARCHAR(255) UNIQUE,
-				enable BOOLEAN,
-				birthdate DATETIME,
-				address LONGTEXT
-			) ENGINE=InnoDB;";
-			$connection->executeStatement($sql);
-			return ['status' => self::SUCCESS, 'message' => "La table '$tableName' a été créée avec succès."];
-		} 
-		catch (\Exception $e) 
-		{
-			return ['status' => self::FAILURE, 'message' => "Erreur lors de la CREATION de la table '$tableName', code erreur : " . $e->getMessage()];
-		}
-	}
+    /**
+     * @Route("/ex00/create_table", name="ex00_create_table")
+     */
+    public function createTable(Connection $connection): Response
+    {
+        $tableName = 'users';
+        $sql = "CREATE TABLE IF NOT EXISTS $tableName (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(25) UNIQUE NOT NULL,
+            name VARCHAR(25) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            enable BOOLEAN NOT NULL DEFAULT 1,
+            birthdate DATETIME NOT NULL,
+            address LONGTEXT NOT NULL
+        )";
+
+        try
+        {
+            if ($this->checkTableExistence($connection, $tableName) === true)
+                $this->addFlash('info', "The table $tableName already exists and cannot be created again.");
+            else
+            {
+                $connection->executeStatement($sql);
+                $this->addFlash('success', "Success! The table $tableName was created!");
+            }
+        }
+        catch (Exception $e)
+        {
+            $this->addFlash('danger', "Error, there was a problem in the table $tableName creation : " . $e->getMessage()); 
+        }
+        return $this->redirectToRoute('ex00_index');
+    }
+
+    private function checkTableExistence(Connection $connection, string $tableName): bool
+    {
+        try
+        {
+            $result = $connection->fetchOne("SHOW TABLES LIKE '$tableName'");
+            return ($result !== false); // Si aucune table n'est trouvée, false est renvoyé (en cas de doute haha).
+        }
+        catch (Exception $e)
+        {
+            return false;
+        }
+    }
 }
