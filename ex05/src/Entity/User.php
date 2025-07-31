3860a234df677ef624b215bd6e78d9818d1a3a54
Winspear22 <adnen.aloui2@hotmail.com>
2025-07-31 16:30:14 +0200

    PHP05 -- ex05 : initialisation.

--- ex05/src/Entity/User.php
+++ ex05/src/Entity/User.php
@@ -20,7 +20,7 @@ class User
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255, unique: true)]
+    #[ORM\Column(length: 255)]
     private ?string $email = null;
 
     #[ORM\Column]
